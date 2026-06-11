7c389e7ea8ab28133269d4d9b1fb5b4e3a29dd75
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2026-06-11 22:02:39 +0800

    basic topic selector

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import Flashcard
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 filename = "static/definitions.json"
 flashcard = Flashcard(filename)
@@ -9,9 +9,20 @@ testables = flashcard.get_testables("physics", "14")
 # init flask app
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        for item in request.form:
+            print(item)
+
+    chapters = [
+            "topic 1",
+            "topic 2",
+            "topic 3",
+            "topic 4",
+            "topic 5",
+            ]
+    return render_template("index.html", chapters=chapters)
 
 if __name__ == "__main__":
     app.run(debug=True)
