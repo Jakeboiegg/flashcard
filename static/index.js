51069fa1353b93a3d46d0127263eb0743c9f65d5
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2026-06-12 13:35:57 +0800

    flipping cards implemented

--- static/index.js
+++ static/index.js
@@ -1,18 +1,21 @@
 console.log(testables)
 
+const card = document.getElementById("card")
 const card_text = document.getElementById("card_text")
 
 const prev_btn = document.getElementById("prev")
 const next_btn = document.getElementById("next")
 
-let index = 0
-
 function update_card() {
-  card_text.textContent = testables[index][0]
+  card_text.textContent = testables[index][flipped]
 }
 
+// init 
+let index = 0
+let flipped = 0
 update_card()
 
+// changing the word
 prev_btn.addEventListener("click", () => {
   index -= 1
   if (index < 0) {
@@ -26,5 +29,11 @@ next_btn.addEventListener("click", () => {
   if (index >= testables.length) {
     index = 0
   }
+  update_card()
+})
+
+// flipping the cards
+card.addEventListener("click", () => {
+  flipped == 0 ? flipped = 1 : flipped = 0 
   update_card()
 })
