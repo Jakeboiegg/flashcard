e5cc0a236464e95ddfe8c740bbb3eaa56ecd4b95
sin <syu3887682@gmail.com>
2026-06-30 22:11:55 +0800

    colission

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from utils import test
 def main():
     filename = "definitions.json"
 
-    test(filename, "physics", "14")
+    # test(filename, "physics", "14")
     # test(filename, "physics", ["11", "12"])
-    # test(filename, "physics", "all")
+    test(filename, "physics", "all")
 
 
 if __name__ == "__main__":
