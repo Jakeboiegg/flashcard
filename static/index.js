875d157003c8765f457bd578977821bd47796882
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2026-07-01 08:33:34 +0800

    implemented shuffle function

--- static/index.js
+++ static/index.js
@@ -5,9 +5,18 @@ const card_text = document.getElementById("card_text")
 
 const prev_btn = document.getElementById("prev")
 const next_btn = document.getElementById("next")
+const shuffle_btn = document.getElementById("shuffle")
 
 function update_card() {
   card_text.textContent = testables[index][flipped]
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 // init 
@@ -15,7 +24,14 @@ let index = 0
 let flipped = 0
 update_card()
 
+// flipping the cards
+card.addEventListener("click", () => {
+  flipped == 0 ? flipped = 1 : flipped = 0 
+  update_card()
+})
+
 // changing the word
+// prev
 prev_btn.addEventListener("click", () => {
   index -= 1
   if (index < 0) {
@@ -24,6 +40,7 @@ prev_btn.addEventListener("click", () => {
   update_card()
 })
 
+// next
 next_btn.addEventListener("click", () => {
   index += 1
   if (index >= testables.length) {
@@ -32,8 +49,10 @@ next_btn.addEventListener("click", () => {
   update_card()
 })
 
-// flipping the cards
-card.addEventListener("click", () => {
-  flipped == 0 ? flipped = 1 : flipped = 0 
+// shuffle
+shuffle_btn.addEventListener("click", ()=> {
+  testables = shuffleArray(testables)
+  console.log(testables)
   update_card()
 })
+
